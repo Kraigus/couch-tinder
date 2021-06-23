05a018208ed194fb68e284cf6d670d2b73761f95
Anton <eliseev.anton96@yandex.ru>
2021-06-23 18:48:34 +0300

    main page rendered

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const { connect } = require("./src/db/config/connect");
-const { dbUrl } = require("./src/db/config/config");
-const morgan = require("morgan");
-const path = require("path");
-const hbs = require("hbs");
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
+const express = require('express');
+const { connect } = require('./src/db/config/connect');
+const { dbUrl } = require('./src/db/config/config');
+const morgan = require('morgan');
+const path = require('path');
+const hbs = require('hbs');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
-const indexRouter = require("./src/routes/index.routes");
+const indexRouter = require('./src/routes/index.routes');
 
 const app = express();
 const PORT = 3000;
 
 connect();
-hbs.registerPartials(path.resolve('views', 'partials'));
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
+hbs.registerPartials(path.join(__dirname, 'src', 'views', 'partials'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(
   session({
-    secret: "kuku1234",
+    secret: 'kuku1234',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false },
@@ -31,7 +31,7 @@ app.use(
   })
 );
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 app.listen(PORT, () => {
   console.log(`Server has ben started on PORT ${PORT}`);
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,7 @@
-const express = require("express");
-const router = require("express").Router();
+const router = require('express').Router();
 
-router
-.route("/")
-.get((req, res) => {
-  res.render("index")
-})
+router.route('/').get((req, res) => {
+  res.render('index');
+});
+
+module.exports = router;
