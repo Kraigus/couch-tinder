17a5c8c4da69448261149029414d9986b8aa997c
neugadal <neugadal@outlook.com>
2021-06-24 16:45:54 +0300

    save before elnar

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
-const User = require("../db/model/user.model")
-const Post = require("../db/model/post.model")
+const User = require("../db/model/user.model");
+const Post = require("../db/model/post.model");
 
-router
-.route("/")
-.get((req, res) => {
+router.route("/").get(async (req, res) => {
+  const coaches = await User.find({ isAdmin: false });
   res.render("index");
 });
 
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const User = require('../db/model/user.model');
+const router = require("express").Router();
+const User = require("../db/model/user.model");
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 router
-  .route('/registration')
+  .route("/registration")
   .get((req, res) => {
-    res.render('registration');
+    res.render("registration");
   })
   .post(async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
@@ -21,48 +21,43 @@ router
       });
       console.log(newUser);
       if (newUser) {
-        console.log(req.session);
-        console.log('oui');
         req.session.firstName = newUser.firstName;
         req.session.lastName = newUser.lastName;
-        console.log(req.session.firstName);
         req.session.userId = newUser._id;
-        res.redirect('/');
+        res.redirect("/");
       }
     } catch (error) {
-      res.redirect('/users/registration');
+      res.redirect("/users/registration");
     }
   });
 
 router
-  .route('/login')
+  .route("/login")
   .get((req, res) => {
-    res.render('login');
+    res.render("login");
   })
   .post(async (req, res) => {
     const { email, password } = req.body;
     console.log(req.body);
     try {
-      console.log(email);
       const findUser = await User.findOne({ email });
-      console.log(findUser);
       const comparePassword = await bcrypt.compare(password, findUser.password);
       if (findUser && comparePassword) {
         req.session.firstName = findUser.firstName;
         req.session.userId = findUser._id;
-        res.redirect('/');
+        res.redirect("/");
       }
     } catch (error) {
       console.log(error);
-      res.redirect('/users/login');
+      res.redirect("/users/login");
     }
   });
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.session.destroy(() => {
-    console.log('Good Bye');
+    console.log("Good Bye");
   });
-  res.redirect('/users/login');
+  res.redirect("/users/login");
 });
 
 module.exports = router;
